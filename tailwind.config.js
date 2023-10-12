5d15432634183e3a0c9463c23b71ada0433ea523
Dhiya <adiadiadha@google.com>
2023-10-12 11:19:23 +0700

    slicing fe login & register

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
         "./resources/**/*.vue",
         "./node_modules/flowbite/**/*.js",
     ],
+
+    // make sure to safelist these classes when using purge
+    safelist: [
+        "w-64",
+        "w-1/2",
+        "rounded-l-lg",
+        "rounded-r-lg",
+        "bg-gray-200",
+        "grid-cols-4",
+        "grid-cols-7",
+        "h-6",
+        "leading-6",
+        "h-9",
+        "leading-9",
+        "shadow-lg",
+  ],
+    
     theme: {
         extend: {},
     },
