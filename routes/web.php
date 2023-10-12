7511f1691e56ee83bd6d7d4254be301b0b764635
Dhiya <adiadiadha@google.com>
2023-10-12 11:25:41 +0700

    slicing dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard-sample', function () {
+    return view('pages.index_sample');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.index');
+});
+
+
